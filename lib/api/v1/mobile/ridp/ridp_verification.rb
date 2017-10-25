7a764ec99b2d905c8b626def72a0260abf96e0c3
krish palaniappan <krish@snowpal.software>
2017-10-25 15:08:59 -0400

    Slug

--- lib/api/v1/mobile/ridp/ridp_verification.rb
+++ lib/api/v1/mobile/ridp/ridp_verification.rb
@@ -67,6 +67,7 @@ module Api
           raise _error_response_message(transaction_id_missing, 422) unless @body[:transaction_id]
           Rails.logger.info "<<RIDP Answer Request>>: #{create_request_payload.call}"
           response = _verification_service_instance.check_override create_request_payload.call
+          raise _error_response_message(ridp_initiate_session_unreachable_error, 503) unless response
           raise _error_response_message(ridp_respond_questions_failure_error(response.transaction_id), 412) if !_response_code_matches(response.response_code, 'SUCCESS')
           _check_user_existence
         end
